7219223b3135f45cdb15975889f1f22fa12e5ba5
Sedat Onat <sedatonat@outlook.com>
2022-11-13 08:03:15 +0300

    Update

--- Tutorial_HWS_100days-d001.playground/Contents.swift
+++ Tutorial_HWS_100days-d001.playground/Contents.swift
@@ -1,30 +1,3 @@
 import Cocoa
 
-var greeting = "Hello, playground"
-
-var name = "Ted"
-name = "Rebecca"
-name = "Keely"
-
-let character = "Daphne"
-
-var playerName = "Roy"
-print(playerName)
-
-playerName = "Dani" // we did not repeat tha var but changed the value
-print(playerName)
-
-playerName = "Sam"
-print(playerName)
-
-let managerName = "Michael Scott"
-let dogBreed = "Samoyed"
-let meaningOfLife = "How many roads a man walk down?"
-
-let movie = """
-A day in
-the life of oan
-Apple engineer
-"""
-// To write multile string, you should both start and end with triple quotes: """
-
+var greeting = "Hello, playground  "
