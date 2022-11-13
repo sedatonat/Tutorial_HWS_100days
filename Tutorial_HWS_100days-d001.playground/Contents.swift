5c7a7904d39c7522fd56d8753ca35662a9e17f60
Sedat Onat <sedatonat@outlook.com>
2022-11-13 07:35:02 +0300

    Update

--- Tutorial_HWS_100days-d001.playground/Contents.swift
+++ Tutorial_HWS_100days-d001.playground/Contents.swift
@@ -16,3 +16,15 @@ print(playerName)
 
 playerName = "Sam"
 print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads a man walk down?"
+
+let movie = """
+A day in
+the life of oan
+Apple engineer
+"""
+// To write multile string, you should both start and end with triple quotes: """
+
