84bfeca3c849a295d62c8db93c213b948d44e4dc
Sedat Onat <sedatonat@outlook.com>
2022-11-13 21:48:56 +0300

    Update

--- Tutorial_HWS_100days-d001.playground/Contents.swift
+++ Tutorial_HWS_100days-d001.playground/Contents.swift
@@ -1,3 +1,21 @@
 import Cocoa
 
-var greeting = "Hello, playground  "
+let actor = "Denzel Washington"
+let filename = "paris.jpg"
+let result = "You win!"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+A day in
+the lof of an
+Apple designer
+"""
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+print(filename.hasSuffix(".jpg"))
