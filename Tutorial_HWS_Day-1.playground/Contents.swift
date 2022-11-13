f1519c56763d10b90c20e9b92f5868f013bc04b5
Sedat Onat <sedatonat@outlook.com>
2022-11-13 07:00:13 +0300

    Initial commit

---
+++ Tutorial_HWS_Day-1.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keely"
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani" // we did not repeat tha var but changed the value
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
