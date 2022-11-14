4182e976d32fccf5f52278f0646c63504eaa5e48
Sedat Onat <sedatonat@outlook.com>
2022-11-14 21:13:04 +0300

    Update

---
+++ Tutorial_HWS_100days-d002.playground/Contents.swift
@@ -0,0 +1,7 @@
+import Cocoa
+
+
+var Celcius = 10.0
+var Fahrenheit = ((Celcius * 9.0 ) / 5.0 ) + 32.0
+var ConversionResult = "The weather is \(Fahrenheit) fahrenheit degrees."
+print (ConversionResult)
