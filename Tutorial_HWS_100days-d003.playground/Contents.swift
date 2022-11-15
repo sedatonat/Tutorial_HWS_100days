65cebd186d04e4e2b00ad01aaf55ce062472df6e
Sedat Onat <sedatonat@outlook.com>
2022-11-15 21:02:08 +0300

    Update

--- Tutorial_HWS_100days-d003.playground/Contents.swift
+++ Tutorial_HWS_100days-d003.playground/Contents.swift
@@ -54,4 +54,61 @@ print(reversedPresidents)
 //print(presidents.reversed())
 // I could not understand how it works #learn
 
+var employee = ["Taylor Swift", "Singer", "Nashville"]
 
+print("Name: \(employee[0])")
+print("Job title: \(employee[1])")
+print("Location: \(employee[2])")
+
+//print("Name: \(employee[0])")
+//employee.remove(at: 1)
+//print("Job title: \(employee[1])")
+//print("Location: \(employee[2])")
+
+
+//let employee2 = ["name": "Taylor Swift", "job": "Singer", "location": "Nashville"]
+let employee2 = [
+    "name": "Taylor Swift",
+    "job": "Singer",
+    "location": "Nashville"
+]
+
+//print(employee2["name"])
+//print(employee2["job"])
+//print(employee2["location"])
+
+
+print(employee2["name", default: "Unknown"])
+print(employee2["job", default: "Unknown"])
+print(employee2["location", default: "Unknown"])
+
+let hasGraduated = [
+    "Eric": false,
+    "Maeve": true,
+    "Otis": false,
+]
+
+
+print("----------------------")
+
+
+let olympics = [
+    2012: "London",
+    2016: "Rio de Janeiro",
+    2021: "Tokyo"
+]
+
+print(olympics[2012, default: "Unknown"])
+print(olympics[2013, default: "Unknown"]) // in error the result is "Unknown" #learn
+
+
+print("----------------------")
+
+
+var archEnemies = [String: String]()
+archEnemies["Batman"] = "The Joker"
+archEnemies["Superman"] = "Lex Luthor"
+archEnemies["Batman"] = "Penguin" // It overwrote the previous Batman value
+
+
+var readings: [Bool] = [false, false, true, false]
