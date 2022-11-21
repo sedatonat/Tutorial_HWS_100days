836c71ade474110a703ce1c13251c4c8bd710f25
Sedat Onat <sedatonat@outlook.com>
2022-11-21 04:31:02 +0300

    update

--- Tutorial_HWS_100days-d008.playground/Contents.swift
+++ Tutorial_HWS_100days-d008.playground/Contents.swift
@@ -32,11 +32,11 @@ func checkPassword(_ password: String) throws -> String {
     if password.count < 5 {
         throw PasswordError.short
     }
-
+    
     if password == "12345" {
         throw PasswordError.obvious
     }
-
+    
     if password.count < 8 {
         return "OK"
     } else if password.count < 10 {
@@ -62,5 +62,44 @@ do {
 
 
 print("-----------------------------------------------------------")
+
+// Checkpoint 4 Test
+
+enum RootError: Error {
+    case tooLow, tooHigh, notFound
+}
+
+
+func squareRoot(of number: Int) throws -> Int {
+    if number < 1 {
+        throw RootError.tooLow
+    }
+
+    if number > 10_000 {
+        throw RootError.tooHigh
+    }
+
+    for i in 1...100 {
+        if i * i == number {
+            return i
+        }
+    }
+    throw RootError.notFound
+}
+
+let number = 6561
+
+do {
+    let root = try squareRoot(of: number)
+    print("The square root of \(number) is \(root).")
+} catch RootError.tooLow {
+    print("Sorry, \(number) is too low – please specify a value from 1 through 10,000.")
+} catch RootError.tooHigh {
+    print("Sorry, \(number) is too high – please specify a value from 1 through 10,000.")
+} catch RootError.notFound {
+    print("Sorry, we could not find an integer square root for \(number).")
+} catch {
+    print("Sorry, there was a problem.")
+}
 
 
