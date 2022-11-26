adebca710c081beaaeb49a75d318cb57b7f16d44
Sedat Onat <sedatonat@outlook.com>
2022-11-26 22:30:52 +0300

    update

---
+++ Tutorial_HWS_100days-d010.playground/Contents.swift
@@ -0,0 +1,40 @@
+import Cocoa
+
+struct Album {
+    let title: String
+    let artist: String
+    let year: Int
+
+    func printSummary() {
+        print("\(title) (\(year)) by \(artist)")
+    }
+}
+
+let red = Album(title: "Red", artist: "Taylor Swift", year: 2012)
+let wings = Album(title: "Wings", artist: "BTS", year: 2016)
+
+print(red.title)
+print(wings.artist)
+
+red.printSummary()
+wings.printSummary()
+
+
+print("--------------------------------------------------")
+
+struct Employee {
+    let name: String
+    var vacationRemaining: Int
+
+    func takeVacation(days: Int) {
+        if vacationRemaining > days {
+            vacationRemaining -= days
+            print("I'm going on vacation!")
+            print("Days remaining: \(vacationRemaining)")
+        } else {
+            print("Oops! There aren't enough days remaining.")
+        }
+    }
+}
+
+
