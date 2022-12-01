cef88347ac1e78bff18f2d420d9f6eb89b288376
Sedat Onat <sedatonat@outlook.com>
2022-12-01 04:24:30 +0300

    update

--- Tutorial_HWS_100days-d010.playground/Contents.swift
+++ Tutorial_HWS_100days-d010.playground/Contents.swift
@@ -93,3 +93,55 @@ struct Investor {
 }
 let investor = Investor(age: 38)
 print(investor.investmentPlan)
+
+
+print("--------------------------------------------------")
+
+//struct Game {
+//    var score = 0
+//}
+//
+//var game = Game()
+//game.score += 10
+//print("Score is now \(game.score)")
+//game.score -= 3
+//print("Score is now \(game.score)")
+//game.score += 1
+//print("Score is now \(game.score)")
+
+print("---")
+
+struct Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var game = Game()
+game.score += 10
+game.score -= 3
+game.score += 1
+
+
+print("--------------------------------------------------")
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+
+        didSet {
+            print("There are now \(contacts.count) contacts.")
+            print("Old value was \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
