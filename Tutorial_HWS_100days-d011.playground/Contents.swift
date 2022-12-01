7de523f141bfac44c69b159e385a3097f2ec54d7
Sedat Onat <sedatonat@outlook.com>
2022-12-01 18:52:01 +0300

    update

--- Tutorial_HWS_100days-d011.playground/Contents.swift
+++ Tutorial_HWS_100days-d011.playground/Contents.swift
@@ -1,3 +1,37 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+var numberpositive: Int = 6
+var numbernegative = -numberpositive
+
+
+print("-------------------------------------")
+
+struct BankAccount {
+    private(set) var funds = 0
+
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+
+    mutating func withdraw(amount: Int) -> Bool {
+        if funds >= amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+var account = BankAccount()
+account.deposit(amount: 100)
+let success = account.withdraw(amount: 70)
+
+if success {
+    print("Withdrew money successfully. Remaining in your account is \(account)")
+} else {
+    print("Failed to get the money")
+}
+
+print(account)
+
