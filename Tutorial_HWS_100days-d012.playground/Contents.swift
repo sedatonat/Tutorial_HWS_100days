74d4ae07b49a5c701fefaf0312ec6c4b2f0531d5
Sedat Onat <sedatonat@outlook.com>
2022-12-05 01:31:32 +0300

    update

--- Tutorial_HWS_100days-d012.playground/Contents.swift
+++ Tutorial_HWS_100days-d012.playground/Contents.swift
@@ -141,6 +141,61 @@ print("---------------------------------------------------------")
 // CHECKPOINT 7
 
 
+class Animal {
+    let legs: Int
 
+    init(legs: Int) {
+        self.legs = legs
+    }
+    
+// This will undeintionally crash the code
+//    func speak () {
+//        assertionFailure("This should never be called.")
+//    }
+    
+}
+
+class Dog: Animal {
+    func speak() {
+        print("Woof!")
+    }
+}
+
+class Corgi: Dog {
+    override func speak() {
+        print("Bark!")
+    }
+}
+
+class Poodle: Dog {
+    override func speak() {
+        print("Yip!")
+    }
+}
+
+class Cat: Animal {
+    let isTame: Bool
+
+    init(legs: Int, isTame: Bool) {
+        self.isTame = isTame
+        super.init(legs: legs)
+    }
+
+    func speak() {
+        print("Meow")
+    }
+}
+
+class Persian: Cat {
+    override func speak() {
+        print("Purr")
+    }
+}
+
+class Lion: Cat {
+    override func speak() {
+        print("Roar!")
+    }
+}
 
 
