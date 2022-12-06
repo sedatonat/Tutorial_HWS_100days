ae90fb1f84d38fa9814a4a6ad2f690060eae31ce
Sedat Onat <sedatonat@outlook.com>
2022-12-06 19:54:46 +0300

    update

--- Tutorial_HWS_100days-d013.playground/Contents.swift
+++ Tutorial_HWS_100days-d013.playground/Contents.swift
@@ -5,8 +5,19 @@ protocol Vehicle {
     func travel(distance: Int)
 }
 
-struct Car: Vehicle {
-    func estimateTime(for distance: Int) -> Int {
+
+func commute(distance: Int, using vehicle: Vehicle) { // protocol made it possible to use it everywhere, no need to create a new func everytime #lesson
+    if vehicle.estimateTime(for: distance) > 100 {
+        print("That's too slow! I'll try a different vehicle.")
+    } else {
+        vehicle.travel(distance: distance)
+    }
+}
+
+
+
+struct Car: Vehicle {  // Not inheriting but comforming to the protocol. All the methods in the protocol must exist in struct, to conform the protocol. This is minimum requirement but you can add to it.
+    func estimateTime(for distance: Int) -> Int {  // between theese paranthesis, the actual work is being done
         distance / 50
     }
 
@@ -19,13 +30,23 @@ struct Car: Vehicle {
     }
 }
 
-func commute(distance: Int, using vehicle: Vehicle) {
-    if vehicle.estimateTime(for: distance) > 100 {
-        print("That's too slow! I'll try a different vehicle.")
-    } else {
-        vehicle.travel(distance: distance)
+
+struct Bicycle: Vehicle {
+    func estimateTime(for distance: Int) -> Int {
+        distance / 10
+    }
+    func travel(distance: Int) {
+        print("I'm cycling \(distance)km")
     }
 }
 
+
+
+
+
 let car = Car()
 commute(distance: 100, using: car)
+
+let bike = Bicycle()
+commute(distance: 50, using: bike)
+
