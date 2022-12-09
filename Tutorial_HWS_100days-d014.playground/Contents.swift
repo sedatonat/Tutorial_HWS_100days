fc62b5d26b8c004eba94cd02e791879c7d0e9779
Sedat Onat <sedatonat@outlook.com>
2022-12-09 21:28:57 +0300

    update

---
+++ Tutorial_HWS_100days-d014.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Cocoa
+
+
+let opposites = [
+    "Mario": "Wario",
+    "Luigi": "Waluigi"
+]
+
+let peachOpposite = opposites["Peach"]
+
+if let marioOpposite = opposites["Mario"] {
+    print("Mario's opposite is \(marioOpposite)")
+}
