9add4dffdf2677514e37bea402c0ad3c4fc389c4
Sedat Onat <sedatonat@outlook.com>
2022-12-09 16:16:56 +0300

    update

--- Tutorial_HWS_100days-d013.playground/Contents.swift
+++ Tutorial_HWS_100days-d013.playground/Contents.swift
@@ -166,3 +166,40 @@ print(user1 != user2)
 
 
 print("----------------------------------------------------------------")
+
+//  CHECKPOINT 8
+
+protocol Building {
+    var type: String { get }
+    var rooms: Int { get }
+    var cost: Int { get set }
+    var agent: String { get set }
+    func printSummary()
+}
+
+extension Building {
+    func printSummary() {
+        print("Talk to \(agent) to buy this \(type) for $\(cost).")
+    }
+}
+
+struct House: Building {
+    let type = "house"
+    var rooms: Int
+    var cost: Int
+    var agent: String
+}
+
+struct Office: Building {
+    let type = "office"
+    var rooms: Int
+    var cost: Int
+    var agent: String
+}
+
+let exampleHouse = House(rooms: 4, cost: 500_000, agent: "Swift & Co")
+exampleHouse.printSummary()
+
+let exampleOffice = Office(rooms: 30, cost: 12_000_000, agent: "Taytay Inc")
+exampleOffice.printSummary()
+
