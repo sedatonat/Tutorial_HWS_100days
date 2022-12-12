92d7b3d68fbb3d58ed9117332bdc0b42cf5241af
Sedat Onat <sedatonat@outlook.com>
2022-12-12 20:56:44 +0300

    update

--- Tutorial_HWS_100days-d014.playground/Contents.swift
+++ Tutorial_HWS_100days-d014.playground/Contents.swift
@@ -35,3 +35,26 @@ print("-------------------------------")
 
 var name: String? = nil
 var age: Int? = nil
+
+
+print("-------------------------------")
+
+func printSquare(of number: Int?) {
+    guard let number = number else {
+        print("Missing input")
+        return
+    }
+
+    print("\(number) x \(number) is \(number * number)")
+}
+
+
+var myVar: Int? = 3
+
+if let unwrapped = myVar {
+    print("Run if myVar has a value inside")
+}
+
+guard let unwrapped = myVar else {
+    print("Run if myVar doesn't have a value inside")
+}
