4b5ad08e63d47fa97503b73bdf5e5994127bf260
Sedat Onat <sedatonat@outlook.com>
2022-12-15 20:44:28 +0300

    update

--- Tutorial_HWS_100days-d015.playground/Contents.swift
+++ Tutorial_HWS_100days-d015.playground/Contents.swift
@@ -2,17 +2,4 @@ import Cocoa
 
 
 
-let employee = [
-    "name": "Taylor",
-    "job": "Singer"
-]
-print(employee["name", default: "Unknown"])
-print(employee["job", default: "Unknown"])
-print(employee["Sedat", default: "Unknown"])
 
-
-enum Weekday {
-    case monday, tuesday, wednesday, thursday, friday
-}
-var day = Weekday.monday
-day = .friday
---
+++ Tutorial_HWS_100days-d015.playground/Sources/Creating constants and variables.swift
@@ -0,0 +1,13 @@
+import Cocoa
+
+//  Swift can create constants and variables, but constants are generally preferable.
+
+//  Use this to create then change a variable string:
+
+var name = "Ted"
+name = "Rebecca"
+//  If you don’t want to change a value, use a constant instead:
+
+let user = "Daphne"
+//  The print() function is helpful for learning and debugging, and shows some information about a variable:
+print(user)
