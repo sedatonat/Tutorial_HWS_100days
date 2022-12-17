ac5712931d344d30d3733253de700e39eb559d93
Sedat Onat <sedatonat@outlook.com>
2022-12-17 21:30:39 +0300

    update

--- HWS_Proj_01 - WeSplit/WeSplit/ContentView.swift
+++ HWS_Proj_01 - WeSplit/WeSplit/ContentView.swift
@@ -12,37 +12,79 @@ struct ContentView: View {
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     
+    @FocusState private var amountIsFocused: Bool
+    
     let tipPercentages = [10, 15, 20, 25, 0]
     
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = (checkAmount / 100) * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Form {
-            
-            Section {
-                TextField(
-                    "Aomunt", // title
-                    value: $checkAmount, // value
-                    format: .currency(code: Locale.current.currency?.identifier ?? "USD")
-                )
-                .keyboardType(.decimalPad)
-                // as default TextField has "text:" but since the value we put into is not a text we had to change it into "value"
-                // "??" helps if currency is not defined so the default is "USD"
+        NavigationView {  // Without it Picker started from 4
+            Form {
+                Section {
+                    TextField(
+                        "Aomunt", // title
+                        value: $checkAmount, // value
+                        format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                    )
+                    .keyboardType(.decimalPad)
+                    // as default TextField has "text:" but since the value we put into is not a text we had to change it into "value"
+                    // "??" helps if currency is not defined so the default is "USD"
+                    .focused($amountIsFocused) // trigers the state
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0-2) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+            header: {
+                Text("How much tip do you want to leave?")
             }
-            
-            Section {
-                Text(
-                    checkAmount,
-                    format: .currency(code: Locale.current.currency?.identifier ?? "USD")
-                )
+                
+                
+                
+                Section {
+                    Text(
+                        totalPerPerson,
+                        format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                    )
+                }
+                
+                
             }
-            
-            
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
     
     
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
