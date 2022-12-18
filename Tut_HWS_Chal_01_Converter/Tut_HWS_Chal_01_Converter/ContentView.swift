e7b60eea580674531ecbd1aeba02dbda184c3fe3
Sedat Onat <sedatonat@outlook.com>
2022-12-18 08:47:52 +0300

    update

--- Tut_HWS_Chal_01_Converter/Tut_HWS_Chal_01_Converter/ContentView.swift
+++ Tut_HWS_Chal_01_Converter/Tut_HWS_Chal_01_Converter/ContentView.swift
@@ -8,15 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var input = 100.0
+    @State private var inputUnit = "Meters"
+    @State private var outputUnit = "Kilometers"
+    
+    let units = ["Feet", "Kilometers", "Meters", "Miles", "Yards"]
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            
+            Section {
+                TextField("Amount", value:$input, format: .number)
+                    .keyboardType(.decimalPad)
+            } header: {
+                Text("Amount to convert")
+            }
+            
+            
+            Picker("Convert from", selection: $inputUnit) {
+                ForEach(units, id: \.self) {
+                    Text($0)
+                }
+            }
+          
+            
+            Picker("Convert to", selection: $inputUnit) {
+                ForEach(units, id: \.self) {
+                    Text($0)
+                }
+            }
+            
+            
         }
-        .padding()
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
