96fa4efb312c391d06d5fc551bc4e29915cc6d43
Sedat Onat <sedatonat@outlook.com>
2022-12-18 10:36:59 +0300

    update

--- Tut_HWS_Chal_01_Converter/Tut_HWS_Chal_01_Converter/ContentView.swift
+++ Tut_HWS_Chal_01_Converter/Tut_HWS_Chal_01_Converter/ContentView.swift
@@ -12,39 +12,98 @@ struct ContentView: View {
     @State private var inputUnit = "Meters"
     @State private var outputUnit = "Kilometers"
     
+    @FocusState private var inputIsFocused: Bool
+    
     let units = ["Feet", "Kilometers", "Meters", "Miles", "Yards"]
+    
+    var result: String {
+        let inputToMetersMultiplier: Double
+        let metersToOutputMultiplier: Double
+        // meters are central location. All the coversion will be made over meters
+ 
+        switch inputUnit {
+        case "Feet":
+            inputToMetersMultiplier = 0.3048  // Feet to Meter
+        case "Kilometers":
+            inputToMetersMultiplier = 1000  // Kilometer to Meter
+        case "Miles":
+            inputToMetersMultiplier = 1609.34  // Mile to Meter
+        case "Yards":
+            inputToMetersMultiplier = 0.9144  // Yard to Meter
+        default:
+            inputToMetersMultiplier = 1  // default value
+    }
+        
+        switch outputUnit {
+        case "Feet":
+            metersToOutputMultiplier = 3.28084  // Meter to Feet
+        case "Kilometers":
+            metersToOutputMultiplier = 0.001  // Meter to Kilometer
+        case "Miles":
+            metersToOutputMultiplier = 0.000621371  // Meter to Mile
+        case "Yards":
+            metersToOutputMultiplier = 1.09361  // Meter to Yard
+        default:
+            metersToOutputMultiplier = 1  // default value
+    }
+
+        let inputInMeters = input * inputToMetersMultiplier
+        let output = inputInMeters * metersToOutputMultiplier
+        
+        let outputString = output.formatted()
+        return "\(outputString) \(outputUnit.lowercased())"
+
+}
     
     
     var body: some View {
         NavigationView {
-            
-            Section {
-                TextField("Amount", value:$input, format: .number)
-                    .keyboardType(.decimalPad)
-            } header: {
-                Text("Amount to convert")
+            Form {
+                Section {
+                    TextField("Amount", value:$input, format: .number)
+                        .keyboardType(.decimalPad)
+                } header: {
+                    Text("Amount to convert")
+                }
+                .keyboardType(.decimalPad)
+                .focused($inputIsFocused) // trigers the state
+                
+                
+                Picker("Convert from", selection: $inputUnit) {
+                    ForEach(units, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                
+                Picker("Convert to", selection: $outputUnit) {
+                    ForEach(units, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                
+                Section {
+                    Text(result)
+                } header: {
+                    Text("Results")
+                }
+                
+                
             }
-            
-            
-            Picker("Convert from", selection: $inputUnit) {
-                ForEach(units, id: \.self) {
-                    Text($0)
+            .navigationTitle("Converter") // It should be attached to Form
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer() // brought Done to the right side
+                    Button("Done") {
+                        inputIsFocused = false
+                    }
                 }
             }
-          
-            
-            Picker("Convert to", selection: $inputUnit) {
-                ForEach(units, id: \.self) {
-                    Text($0)
-                }
-            }
-            
-            
         }
     }
-    
-    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
