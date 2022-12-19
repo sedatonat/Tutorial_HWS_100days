614efb7d74876b8ab74216abfea46ca5de40107a
Sedat Onat <sedatonat@outlook.com>
2022-12-19 05:08:58 +0300

    update
    Removed recursive questions

--- Tut_HWS_Proj_02_GuessTheFlag/v2/Guess The Flasg v1/ContentView.swift
+++ Tut_HWS_Proj_02_GuessTheFlag/v2/Guess The Flasg v1/ContentView.swift
@@ -8,16 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var questionCounter = 1
     @State private var showingScore = false
+    @State private var showingResults = false
     @State private var scoreTitle = ""
     @State private var score = 0
     
     
-    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]  // these should match the assets
-        .shuffled()
+    @State private var countries = allCountries.shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
-    
+    static let allCountries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]  // these should match the assets
     
     var body: some View {
         
@@ -83,11 +84,19 @@ struct ContentView: View {
                 
         }
         // End of ZStack
+        
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
             Text("Your score is \(score)")
         }
+        
+        .alert("Game Over", isPresented: $showingResults) {
+            Button("Start Again", action: newGamme)
+        } message: {
+            Text("Your final score was \(score)")
+        }
+        
         
         
     }
@@ -99,15 +108,39 @@ struct ContentView: View {
             scoreTitle = "Correct"
             score += 1
         } else {
-            scoreTitle = "Wrong"
+            let needsThe = ["UK", "US"]
+            let theirAnswer = countries[number]
+            if needsThe.contains(theirAnswer) {
+                scoreTitle = "Wrong! That's the flag of the \(countries[number])"
+            } else {
+                scoreTitle = "Wrong! That's the flag of \(countries[number])"
+            }
+            
             if score > 0 { score -= 1 }
         }
-        showingScore = true  // Regardless from result show the score
+        
+        
+        if questionCounter == 8 {
+            showingResults = true
+        } else {
+            showingScore = true
+        }
+        
+        
     }
     
     func askQuestion() {
+        countries.remove(at: correctAnswer)
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        questionCounter += 1
+    }
+    
+    func newGamme() {
+        questionCounter = 0
+        score = 0
+        countries = Self.allCountries
+        askQuestion()
     }
     
 }
