3f028f79036f25bb233c997036fcf2535a50e538
Sedat Onat <sedatonat@outlook.com>
2022-12-23 19:39:49 +0300

    update

--- Tut_HWS_Proj_04_BetterRest/BetterRest/ContentView.swift
+++ Tut_HWS_Proj_04_BetterRest/BetterRest/ContentView.swift
@@ -9,14 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
     
     
     var body: some View {
-        Stepper("\(sleepAmount) hours", value: $sleepAmount)
+        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
         
+        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)  // Select only future dates
+//            .labelsHidden()  // VoiceOver will read it you can't see the label
+        
+//        func exampleDate() {
+//            let tomorrow = Date.now.addingTimeInterval(86400)
+//            let range = Date.now...tomorrow
+        }
         
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
