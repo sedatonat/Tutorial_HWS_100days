7d986164773894dd0212c1925f244a354e5b141d
Sedat Onat <sedatonat@outlook.com>
2022-12-24 05:21:09 +0300

    update

--- Tut_HWS_Proj_04_BetterRest/BetterRest/ContentView.swift
+++ Tut_HWS_Proj_04_BetterRest/BetterRest/ContentView.swift
@@ -8,22 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var cofeeeAmount = 1
     
     
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-        
-        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)  // Select only future dates
-//            .labelsHidden()  // VoiceOver will read it you can't see the label
-        
-//        func exampleDate() {
-//            let tomorrow = Date.now.addingTimeInterval(86400)
-//            let range = Date.now...tomorrow
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()  // Hides the label
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily cofee intake")
+                    .font(.headline)
+                
+                Stepper(cofeeeAmount == 1 ? "1 cup" : "\(cofeeeAmount) cups", value: $cofeeeAmount, in: 1...20)
+                
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
+            
         }
+    }
+    
+    func calculateBedTime() {
+     
         
     }
+    
+}
 
 
 struct ContentView_Previews: PreviewProvider {
