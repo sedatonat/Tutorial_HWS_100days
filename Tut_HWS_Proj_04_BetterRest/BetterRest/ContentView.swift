46090190e752d47691142d9329dae414cd42ea88
Sedat Onat <sedatonat@outlook.com>
2022-12-25 06:18:07 +0300

    update

--- Tut_HWS_Proj_04_BetterRest/BetterRest/ContentView.swift
+++ Tut_HWS_Proj_04_BetterRest/BetterRest/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Sedat Onat on 22.12.2022.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
@@ -12,6 +13,9 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var cofeeeAmount = 1
     
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
         NavigationView {
@@ -37,13 +41,37 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: calculateBedTime)
             }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") { } // no action, just dismiss
+            } message: {
+                Text(alertMessage)
+            }
             
         }
     }
     
     func calculateBedTime() {
-     
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60  // "?? 0" is for just in case we cannot read the value
+            let minute = (components.minute ?? 0) * 60  // "?? 0" is for just in case we cannot read the value
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(cofeeeAmount))  // Since prediction model works in double number format, we had to change the formats to the double
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+            
+        } catch {  // ctach errors
+            alertTitle = "Errors"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
         
+        showingAlert = true
     }
     
 }
