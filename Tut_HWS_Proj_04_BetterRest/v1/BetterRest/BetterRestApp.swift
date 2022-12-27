0981ccd3bca2111cd5dfd2a0f8c54d61f99d67f2
Sedat Onat <sedatonat@outlook.com>
2022-12-27 04:11:09 +0300

    update

---
+++ Tut_HWS_Proj_04_BetterRest/v1/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sedat Onat on 22.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tut_HWS_Proj_04_BetterRest/v1/BetterRest/ContentView.swift
@@ -0,0 +1,100 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Sedat Onat on 22.12.2022.
+//
+
+import CoreML
+import SwiftUI
+
+struct ContentView: View {
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var cofeeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    // reading a property from another property will cinfuse Swift becuase it cannot know the priority of it. Statis helps us in this
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                
+                VStack(alignment: .leading, spacing:0) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()  // Hides the label
+                }
+                
+                VStack(alignment: .leading, spacing:0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+                
+                VStack(alignment: .leading, spacing:0) {
+                    Text("Daily cofee intake")
+                        .font(.headline)
+                    
+                    Stepper(cofeeeAmount == 1 ? "1 cup" : "\(cofeeeAmount) cups", value: $cofeeeAmount, in: 1...20)
+                }
+                
+                .navigationTitle("BetterRest")
+                .toolbar {
+                    Button("Calculate", action: calculateBedTime)
+                }
+                .alert(alertTitle, isPresented: $showingAlert) {
+                    Button("OK") { } // no action, just dismiss
+                } message: {
+                    Text(alertMessage)
+                }
+                
+            }
+        }
+    }
+    
+    func calculateBedTime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60  // "?? 0" is for just in case we cannot read the value
+            let minute = (components.minute ?? 0) * 60  // "?? 0" is for just in case we cannot read the value
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(cofeeeAmount))  // Since prediction model works in double number format, we had to change the formats to the double
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+            
+        } catch {  // ctach errors
+            alertTitle = "Errors"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        
+        showingAlert = true
+    }
+    
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
