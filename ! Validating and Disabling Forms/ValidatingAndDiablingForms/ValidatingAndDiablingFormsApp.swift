12c051e2821fc4ee676a2697fd4d833ec11f99b7
Sedat Onat <sedatonat@outlook.com>
2023-01-11 05:13:51 +0300

    update

---
+++ ! Validating and Disabling Forms/ValidatingAndDiablingForms/ValidatingAndDiablingFormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValidatingAndDiablingFormsApp.swift
+//  ValidatingAndDiablingForms
+//
+//  Created by Sedat Onat on 11.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ValidatingAndDiablingFormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
